044f9d8d0b930009f6f3259d59fb2e52f47e1237
Rakel Nordfjord <raknord@gmail.com>
2014-10-18 21:46:30 +0000

    added test5 for newline

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -3,16 +3,14 @@ package is.ru.stringcalculator;
 public class Calculator {
 
 	public static int add(String text){
+				
 		if(text.equals("")){
 			return 0;
 		}
-		else if(text.contains(",")) {
-			String[] numbers = text.split(",");
-			int sum = 0;
-			for(String n : numbers) {
-				sum += toInt(n);
-			}
-			return sum;
+		if(text.contains(",") || text.contains("\n")) {
+			text = text.replace("\n", ",");
+			String[] num = splitUp(text);	
+			return sum(num);
 		}
 		else
 			return toInt(text); 
@@ -20,6 +18,18 @@ public class Calculator {
 	}
 	private static int toInt(String n) {
 		return Integer.parseInt(n);
+	}
+
+	private static String[] splitUp(String n) {
+		return n.split(",");
+	}
+
+	private static int sum(String[] n) {
+		int summa = 0;
+		for(String num : n) {
+			summa += toInt(num);
+		}
+		return summa;
 	}
 }
 
--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -27,4 +27,9 @@ public class CalculatorTest {
 	public void testManyNumbers() {
 		assertEquals(6, Calculator.add("1,2,3"));
 	}
+
+	@Test
+	public void testNewlineNumbers() {
+		assertEquals(6, Calculator.add("1\n2,3"));
+	}
 }  
